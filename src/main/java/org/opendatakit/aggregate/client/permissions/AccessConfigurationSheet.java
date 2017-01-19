91d19bc262b664369fbebe2c1f65e7abee2698cd
Jakub Redlarski <jredlarski@soldevelo.com>
2017-01-19 14:00:58 +0100

    ODK-49 Added missing import
    Added missing java.util.Arrays import to the AccessConfigurationSheet.java

--- src/main/java/org/opendatakit/aggregate/client/permissions/AccessConfigurationSheet.java
+++ src/main/java/org/opendatakit/aggregate/client/permissions/AccessConfigurationSheet.java
@@ -17,6 +17,7 @@
 package org.opendatakit.aggregate.client.permissions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
