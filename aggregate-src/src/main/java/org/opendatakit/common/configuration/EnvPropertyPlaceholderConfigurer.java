da252ec92eea82962d2142b6a87279498be68ec5
Caden Howell <cadenh@benetech.org>
2017-02-16 15:48:42 -0600

    ODK-93 Let spring config variables be read from environment variable

---
+++ aggregate-src/src/main/java/org/opendatakit/common/configuration/EnvPropertyPlaceholderConfigurer.java
@@ -0,0 +1,21 @@
+package org.opendatakit.common.configuration;
+
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+public class EnvPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+
+  private static final Log logger = LogFactory.getLog(EnvPropertyPlaceholderConfigurer.class.getName());
+
+  @Override
+  protected String resolvePlaceholder(String placeholder, Properties props) {
+    String envVar = placeholder.replace(".", "_").toUpperCase();
+    String result = System.getenv(envVar);
+    logger.info("Checking " + placeholder + " as " + envVar + " and finding " + result);
+    return StringUtils.isNotEmpty(result) ? result : super.resolvePlaceholder(placeholder, props);
+  }
+}
