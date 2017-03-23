11b5ce406d4e7bded84431cc0b8948bd37352c69
Caden Howell <cadenh@benetech.org>
2017-03-23 15:59:49 -0500

    ODK-118 Move Wink to Spring config, override

--- aggregate-src/src/main/java/org/opendatakit/common/configuration/WinkEnvPropertiesFactoryBean.java
+++ aggregate-src/src/main/java/org/opendatakit/common/configuration/WinkEnvPropertiesFactoryBean.java
@@ -9,9 +9,13 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 
 /**
- * 
- * Here we're bending over backwards to
+ * Here we're bending over backwards to override the URL used to generate links in the JSON.
+ * If EXTERNAL_ROOT_URL is set, that indicates that we have an external proxy server (or some other 
+ * reason) and we want all future requests based on outgoing data to come back through EXTERNAL_ROOT_URL 
+ * and not the server address Wink finds through self inspection in UriInfoImpl.buildBaseUriString
  *
+ * wink.http.uri and wink.https.uri can also be set in wink.properties, but will be overridden by
+ * the EXTERNAL_ROOT_URL environment variable by EnvPropertyPlaceholder.
  */
 public class WinkEnvPropertiesFactoryBean extends PropertiesFactoryBean {
 
